50e817cf5cb88265cf5caf6abd202495897c544a
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2017-01-18 13:48:00 +0100

    Add segment to reserve AP startup page

--- src/lapic.cpp
+++ src/lapic.cpp
@@ -84,9 +84,9 @@ void Lapic::init()
 
         trace (TRACE_APIC, "TSC:%u kHz BUS:%u kHz", freq_tsc, freq_bus);
 
-        send_ipi (0, 1, DLV_SIPI, DSH_EXC_SELF);
+        send_ipi (0, AP_BOOT_PADDR >> PAGE_BITS, DLV_SIPI, DSH_EXC_SELF);
         Acpi::delay (1);
-        send_ipi (0, 1, DLV_SIPI, DSH_EXC_SELF);
+        send_ipi (0, AP_BOOT_PADDR >> PAGE_BITS, DLV_SIPI, DSH_EXC_SELF);
     }
 
     write (LAPIC_TMR_ICR, 0);
